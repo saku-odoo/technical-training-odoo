fd234d6a362c6e5be5bf938472d5c940f0b460d1
saku-odoo <saku@odoo.com>
2023-10-11 17:10:34 +0530

    test commit

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name":"Motorcycle Registry",
+    "summary":"Manage Registration of Motorcycles",
+    "description":"""
+        This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycle of the brand.
+        """,
+    "author":"saku-odoo",
+    "version":"16.0",
+    "category":"kawiil",
+    "website":"",
+    "application": True,
+    "depends": ["base"],
+    "installable": True,
+    'license': 'LGPL-3',
+}
